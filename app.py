4f7bf8837b647e36f1d934ea0bd882d1be639b49
YenCChien <uu771216@gmail.com>
2018-09-19 17:30:03 +0800

    update

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+from datetime import datetime
 import pandas as pd
 import flask
 import plotly.plotly as py
@@ -15,49 +16,32 @@ app.layout = html.Div(
     [
         # header
         html.Div([
-
-            html.Span("CRM App using Salesforce API", className='app-title'),
-            
-            html.Div(
-                html.Img(src='https://github.com/YenCChien/mfg-center/blob/master/img/logo_.png',height="100%")
-                ,style={"float":"right","height":"100%"})
+            html.Span("D70-MFG Center", className='app-title'),
+            # html.Div(
+            #     html.Img(src='https://github.com/YenCChien/mfg-center/blob/master/img/logo.png',height="100%")
+            #     ,style={"float":"right","height":"100%"})
             ],
             className="row header"
             ),
-
         # tabs
         html.Div([
-
             dcc.Tabs(
                 id="tabs",
                 style={"height":"20","verticalAlign":"middle"},
                 children=[
-                    dcc.Tab(label="Opportunities", value="opportunities_tab"),
-                    dcc.Tab(label="Leads", value="leads_tab"),
-                    dcc.Tab(id="cases_tab",label="Cases", value="cases_tab"),
+                    dcc.Tab(label="A", value="a_tab"),
+                    dcc.Tab(label="AFI", value="afi_tab"),
+                    dcc.Tab(id="cases_tab",label="B", value="b_tab"),
                 ],
-                value="leads_tab",
+                value="afi_tab",
             )
 
             ],
             className="row tabs_div"
             ),
-       
-                
-        # divs that save dataframe for each tab
-        # html.Div(
-        #         sf_manager.get_opportunities().to_json(orient="split"),  # opportunities df
-        #         id="opportunities_df",
-        #         style={"display": "none"},
-        #     ),
-        # html.Div(sf_manager.get_leads().to_json(orient="split"), id="leads_df", style={"display": "none"}), # leads df
-        # html.Div(sf_manager.get_cases().to_json(orient="split"), id="cases_df", style={"display": "none"}), # cases df
-
-
-
+        
         # Tab content
         html.Div(id="tab_content", className="row", style={"margin": "2% 3%"}),
-        
         html.Link(href="https://use.fontawesome.com/releases/v5.2.0/css/all.css",rel="stylesheet"),
         html.Link(href="https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css",rel="stylesheet"),
         html.Link(href="https://fonts.googleapis.com/css?family=Dosis", rel="stylesheet"),
@@ -69,15 +53,74 @@ app.layout = html.Div(
     style={"margin": "0%"},
 )
 
+def indicator(color, text, id_value):
+    return html.Div(
+        [
+            html.P(
+                text,
+                className="twelve columns indicator_text"
+            ),
+            html.P(
+                id = id_value,
+                className="indicator_value"
+            ),
+        ],
+        className="four columns indicator",
+    )
+
+def afiTab():
+    return html.Div([
+            dcc.DatePickerRange(
+                id='date-picker-range',
+                start_date=datetime.now(),
+                end_date_placeholder_text='Select a date!'
+            ),
+            html.Div([
+            html.Div(
+                [
+                    html.P("Leads count per state"),
+                    dcc.Graph(
+                        id="map",
+                        style={"height": "90%", "width": "98%"},
+                        config=dict(displayModeBar=False),
+                    ),
+                ],
+                className="four columns chart_div"
+            ),
+
+            html.Div(
+                [
+                    html.P("Leads by source"),
+                    dcc.Graph(
+                        id="lead_source",
+                        style={"height": "90%", "width": "98%"},
+                        config=dict(displayModeBar=False),
+                        figure=dict(data=[go.Pie(
+                            labels=['a','b','c','d','e','f'],
+                            values=[2,3,5,7,10,8],
+                            marker={"colors": ["#264e86", "#0074e4", "#74dbef", "#eff0f4"]},
+                            )], layout=dict(margin=dict(l=0, r=0, t=0, b=65), legend=dict(orientation="h")))
+                    ),
+                ],
+                className="four columns chart_div"
+            ),
+            indicator(
+                "#EF553B",
+                "Conversion Rates",
+                "right_leads_indicator",
+            ),
+
+            ])
+        ])
 
 @app.callback(Output("tab_content", "children"), [Input("tabs", "value")])
 def render_content(tab):
-    if tab == "opportunities_tab":
-        return opportunities.layout
-    elif tab == "cases_tab":
-        return cases.layout
-    elif tab == "leads_tab":
-        return leads.layout
+    if tab == "a_tab":
+        return 'No Content'
+    elif tab == "b_tab":
+        return 'No Content'
+    elif tab == "afi_tab":
+        return afiTab()
     else:
         return opportunities.layout
 
