f615f41db1aa9718b0272d67a109954b87203aff
fboot <31297491+YenCChien@users.noreply.github.com>
2018-12-03 08:34:29 +0800

    Update cpk.py

--- cpk.py
+++ cpk.py
@@ -2,6 +2,7 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.figure_factory as ff
 import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
@@ -12,6 +13,13 @@ import math
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from app import app
+import numpy as np
+from datetime import datetime
+from mongo import *
+
+mPass = []
+for m in range(1,13):
+    mPass.append(monthPass(m))
 
 def indicator(color, text, id_value):
     return html.Div(
@@ -32,24 +40,75 @@ def cpkTab():
     return html.Div([
         dcc.DatePickerRange(
             id='date-picker',
-            start_date=datetime.datetime.now()-relativedelta(months=1),
+            start_date=datetime.now()-relativedelta(months=1),
             end_date_placeholder_text='Select a date!'
         ),
-    html.Div([
-        html.Div(
-	        id="leads_table",
-	        className="row",
-	        style={
-	            "maxHeight": "600px",
-	            "overflowY": "scroll",
-	            "padding": "4",
-	            "marginTop": "10",
-	            "backgroundColor":"white",
-	            "border": "1px solid #C8D4E3",
-	            "borderRadius": "3px"
-	        },
-	    ),
-    ])
+        html.Div([
+            html.Div(
+                [
+                    html.P("Leads count per state"),
+                    dcc.Graph(
+                        id='displot',
+                        style={"height": "90%", "width": "98%"},
+                        figure=ff.create_distplot([np.random.randn(100),np.random.randn(100)], ['2018','2019'], bin_size=.5),
+                        config=dict(displayModeBar=False)
+                    ),
+                ],
+                className="four columns chart_div"
+            ),
+
+            html.Div(
+                [
+                    html.P("Leads by source"),
+                    dcc.Graph(
+                        id="lead_source",
+                        style={"height": "90%", "width": "98%"},
+                        config=dict(displayModeBar=False),
+                        figure=dict(data=[go.Pie(
+                            labels=['a','b','c','d','e','f'],
+                            values=[2,3,5,7,10,8],
+                            marker={"colors": ["#264e86", "#0074e4", "#74dbef", "#eff0f4"]},
+                            )], layout=dict(margin=dict(l=0, r=0, t=0, b=65), legend=dict(orientation="h")))
+                    ),
+                ],
+                className="four columns chart_div"
+            ),
+
+            html.Div(
+                [
+                    html.P("Leads count per state"),
+                    dcc.Graph(
+                        id="map",
+                        style={"height": "90%", "width": "98%"},
+                        config=dict(displayModeBar=False),
+                        figure=dict(
+                            data=[go.Bar(
+                            x=mPass,
+                            y=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+                            name= 'SF',
+                            orientation = 'h'
+                        )],
+                        layout=dict(barmode='stack',margin=dict(l=100,r=25,b=20,t=0,pad=4),paper_bgcolor='white',plot_bgcolor='white')
+                        )
+                    ),
+                ],
+                className="four columns chart_div"
+            ),
+
+            html.Div(
+    	        id="leads_table",
+    	        className="row",
+    	        style={
+    	            "maxHeight": "300px",
+    	            "overflowY": "scroll",
+    	            "padding": "4",
+    	            "marginTop": "10",
+    	            "backgroundColor":"white",
+    	            "border": "1px solid #C8D4E3",
+    	            "borderRadius": "3px"
+    	        },
+    	    ),
+        ])
     ])
 
 def df_to_table(df):
@@ -68,3 +127,4 @@ def df_to_table(df):
             for i in range(len(df))
         ]
     )
+
